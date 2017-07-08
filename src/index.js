5441ce50ecd1dff61bf097e539785c9ad4e6a7b3
Omar Reyes <oreyes@homeaway.com>
2017-07-08 17:10:44 -0500

    Added files to explain react elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {containerElement} from './what-is-react-fundamental-concepts/elements';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(containerElement, document.getElementById('root'));
 registerServiceWorker();
